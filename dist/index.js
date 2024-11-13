5161be4f76a100525fa247b821b2d00bbdf0ff44
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-11-13 12:18:07 +0300

    feat: Header layout has been done

--- dist/index.js
+++ dist/index.js
@@ -1128,7 +1128,20 @@
       this.el.classList.add('header');
       this.el.innerHTML = `
       <div>
-        <img src="/static/logo.svg" alt="logo" class="header__img" />
+        <img src="/static/logo.svg" alt="logo" class="header__logo" />
+      </div>
+      <div class="menu">
+        <a class="menu__item" href="#">
+          <img src="/static/search.svg" alt="search" />
+          Search books
+        </a>
+        <a class="menu__item" href="#favorites">
+          <img src="/static/favorites.svg" alt="favorites" />
+          Favorites
+        </a>
+        <div class="menu__counter">
+          ${this.appState.favorites.length}
+        </div>
       </div>
     `;
 
